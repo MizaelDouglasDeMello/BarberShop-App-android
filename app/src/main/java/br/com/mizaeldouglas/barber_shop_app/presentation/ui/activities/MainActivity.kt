2d341c43c79a7764ab458c25e7f14fdcc7bcec1e
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-11-24 22:03:28 -0300

    Initial commit

---
+++ app/src/main/java/br/com/mizaeldouglas/barber_shop_app/presentation/ui/activities/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.com.mizaeldouglas.barber_shop_app.presentation.ui.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.mizaeldouglas.barber_shop_app.presentation.ui.theme.BarberShopAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BarberShopAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BarberShopAppTheme {
+        Greeting("Android")
+    }
+}
